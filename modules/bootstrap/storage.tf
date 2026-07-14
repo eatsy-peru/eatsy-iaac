878fb9d804019230a54d07635592211dbff40c37
Genaro de Pomar <gdpc215@gmail.com>
2026-07-13 19:00:14 -0500

    fix: Correct storage account reference in certificate storage container configuration

--- modules/bootstrap/storage.tf
+++ modules/bootstrap/storage.tf
@@ -33,6 +33,6 @@ resource "azurerm_storage_account" "tfstate" {
 # Certificate Storage Container
 resource "azurerm_storage_container" "certs" {
   name                  = "certs"
-  storage_account_name  = azurerm_storage_account.tfstate.name
+  storage_account_id    = azurerm_storage_account.tfstate.id
   container_access_type = "private"
 }
